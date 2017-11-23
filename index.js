87efd5e80bb4d54284f69053056d7440ffc1e3a4
Arno Vande Cappelle <arnovdc@msn.com>
2017-11-23 14:45:38 +0100

    ussless

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var nba = require("nba");
+var fs = require('fs');
 
 var players = nba.players;
 
@@ -6,12 +7,27 @@ var bobTheJSONBuilder;
 
 //console.log(players);
 
-nba.stats.playerInfo({PlayerID: 201939})
+nba.stats.playerInfo({PlayerID: 201939}).then(function(result) {
+  console.log(result)
+
+  for(var i = 0; i < result.availableSeasons.length; i++){
+    //console.log(result.availableSeasons[i]);
+
+    nba.stats.playerSplits({ PlayerID: 201939 }).then(function(stats){
+      console.log("before stats save");
+      fs.writeFile("./tmp/test.json", stats.tostring(), function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log("The file was saved!");
+
+    }); 
+    }, function(err){
+      console.log(err);
+    });
+    
+  }
+
+})
 
 
-nba.stats.playerSplits({ PlayerID: 201939 }).then(function(result){
-  console.log("now");
-  console.log(result);
-}, function(err){
-  console.log(err);
-});
