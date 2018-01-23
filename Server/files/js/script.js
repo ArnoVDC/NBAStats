d82a048847c2938257e82b9d2f84d08e09ab975d
Ahmed Ben Ali <ahmed.ben.ali@student.howest.be>
2018-01-23 22:12:49 +0100

    oik

--- Server/files/js/script.js
+++ Server/files/js/script.js
@@ -3,12 +3,39 @@ $(document).ready(function () {
     
     $.getJSON('./data/jsonSchools.JSON', function(data){
             $select.html('');
-            console.log(data[123]);
+
 
             for(var i =0; i< data.length; i++){
               $select.append('<option id="'+ data[i]['Id'] + '">' +data[i]['School'] + '</option>');
             }
 
     });
+
+    $("#FormDraft").submit(function(event) {
+      
+                      /* stop form from submitting normally */
+                      event.preventDefault();
+      
+                      /* get some values from elements on the page: */
+                      var $form = $(this),
+                          term = $form.find('input[name="blocks"]').val(),
+                          url = $form.attr('action');
+                          console.log("ok")
+                      /* Send the data using post */
+                      var posting = $.post(url, {
+                          s: term
+                          
+                      });
+                        console.log($form )
+                      /* Put the results in a div */
+                      posting.done(function(data) {
+                        console.log('ok')
+                          var content = $(data).find('#content');
+                         
+                          console.log(content)
+                      });
+                  });
+
   });
 
+  
