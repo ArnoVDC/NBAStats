0ae81619918d96d3664a5633268cf0749240b3a2
Ahmed Ben Ali <ahmed.ben.ali@student.howest.be>
2018-01-23 23:59:21 +0100

    fupdate

--- Server/files/js/script.js
+++ Server/files/js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     console.log(data[123]);
 
     for (var i = 0; i < data.length; i++) {
-      $select.append('<option id="' + data[i]['Id'] + '">' + data[i]['School'] + '</option>');
+      $select.append('<option id="' + data[i]['Id']  + '" value="'+data[i]['Id']+'" >' + data[i]['School'] + '</option>');
     }
 
   });
@@ -15,12 +15,12 @@ $(document).ready(function () {
 function post(form) {
   var http = new XMLHttpRequest();
   var url = "";
-  var params = school=document.getElementById("school").value;G=document.getElementById("G").value;MP=document.getElementById("MP").value;
-  PTS=document.getElementById("PTS").value;TRB=document.getElementById("TRB").value,AST=document.getElementById("AST").value,STL=document.getElementById("STL").value;
-  BLK=document.getElementById("BLK").value;
-  FG=document.getElementById("FG").value;P2=document.getElementById("2P").value;P3=document.getElementById("3P").value;
-  FT=document.getElementById("FT").value;
-  WS=document.getElementById("WS").value;WS=document.getElementById("WS1").value;
+  var school = document.getElementById("school").options;
+  var params = "school="+document.getElementById("school").value+";"+"G="+document.getElementById("G").value+";"+"MP="+document.getElementById("MP").value+";"
+  +"PTS="+document.getElementById("PTS").value+";TRB="+document.getElementById("TRB").value+";AST="+document.getElementById("AST").value+";"
+  +"STL="+document.getElementById("STL").value+";BLK="+document.getElementById("BLK").value+";"
+  +"FG="+document.getElementById("FG").value+";P2="+document.getElementById("2P").value+"P3="+document.getElementById("3P").value+";"
+  +"FT="+document.getElementById("FT").value+";WS="+document.getElementById("WS").value+";WS="+document.getElementById("WS1").value+";";
   http.open("POST", url, true);
   http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   http.onreadystatechange = function () {//Call a function when the state changes.
