5ddb1e7bfb2580f5818647bd4a7d3f7aca9de1b2
Arno Vande Cappelle <arnovdc@msn.com>
2018-01-24 00:21:19 +0100

    return the right output form the backend

--- Server/files/js/script.js
+++ Server/files/js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     console.log(data[123]);
 
     for (var i = 0; i < data.length; i++) {
-      $select.append('<option id="' + data[i]['Id']  + '" value="'+data[i]['Id']+'" >' + data[i]['School'] + '</option>');
+      $select.append('<option id="' + data[i]['Id'] + '" value="' + data[i]['Id'] + '" >' + data[i]['School'] + '</option>');
     }
 
   });
@@ -16,17 +16,25 @@ function post(form) {
   var http = new XMLHttpRequest();
   var url = "";
   var school = document.getElementById("school").options;
-  var params = "school="+document.getElementById("school").value+";"+"G="+document.getElementById("G").value+";"+"MP="+document.getElementById("MP").value+";"
-  +"PTS="+document.getElementById("PTS").value+";TRB="+document.getElementById("TRB").value+";AST="+document.getElementById("AST").value+";"
-  +"STL="+document.getElementById("STL").value+";BLK="+document.getElementById("BLK").value+";"
-  +"FG="+document.getElementById("FG").value+";P2="+document.getElementById("2P").value+"P3="+document.getElementById("3P").value+";"
-  +"FT="+document.getElementById("FT").value+";WS="+document.getElementById("WS").value+";WS="+document.getElementById("WS1").value+";";
+  var params = "school=" + document.getElementById("school").value +
+    ";G=" + document.getElementById("G").value +
+    ";FG=" + document.getElementById("FG").value +
+    ";P2=" + document.getElementById("2P").value +
+    ";P3=" + document.getElementById("3P").value +
+    ";FT=" + document.getElementById("FT").value +
+    ";TRB=" + document.getElementById("TRB").value +
+    ";AST=" + document.getElementById("AST").value +
+    ";STL=" + document.getElementById("STL").value +
+    ";BLK=" + document.getElementById("BLK").value +
+    ";PTS=" + document.getElementById("PTS").value + ";"
+
+    
   http.open("POST", url, true);
   http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   http.onreadystatechange = function () {//Call a function when the state changes.
     if (http.readyState == 4 && http.status == 200) {
       alert(http.responseText);
-      
+
     }
   }
   http.send(params);
