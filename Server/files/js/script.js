e37a1664bb6cf02f8f8213a130839d112508d41d
Ahmed Ben Ali <ahmed.ben.ali@student.howest.be>
2018-01-23 19:37:17 +0100

    update

--- Server/files/js/script.js
+++ Server/files/js/script.js
@@ -1,22 +1,14 @@
-console.log("loaded");
+$(document).ready(function () {
+    var $select = $('#school');
+    
+    $.getJSON('./data/jsonSchools.JSON', function(data){
+            $select.html('');
+            console.log(data[123]);
 
-function post() {
-  var http = new XMLHttpRequest();
-  var url = "";
-  var params = "data=hello";
-  http.open("POST", url, true);
+            for(var i =0; i< data.length; i++){
+              $select.append('<option id="'+ data[i]['Id'] + '">' +data[i]['School'] + '</option>');
+            }
 
-  //Send the proper header information along with the request
-  http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    });
+  });
 
-  http.onreadystatechange = function () {//Call a function when the state changes.
-    if (http.readyState == 4 && http.status == 200) {
-      alert(http.responseText);
-    }
-  }
-  http.send(params);
-}
-
-post();
-
-console.info("posted");
