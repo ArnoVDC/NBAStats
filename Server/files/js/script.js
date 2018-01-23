1e607cb9695ae04e90535e847fde61c33d81be35
Arno Vande Cappelle <arnovdc@msn.com>
2018-01-23 21:56:34 +0100

    added post function

--- Server/files/js/script.js
+++ Server/files/js/script.js
@@ -1,14 +1,29 @@
 $(document).ready(function () {
-    var $select = $('#school');
-    
-    $.getJSON('./data/jsonSchools.JSON', function(data){
-            $select.html('');
-            console.log(data[123]);
+  var $select = $('#school');
 
-            for(var i =0; i< data.length; i++){
-              $select.append('<option id="'+ data[i]['Id'] + '">' +data[i]['School'] + '</option>');
-            }
+  $.getJSON('./data/jsonSchools.JSON', function (data) {
+    $select.html('');
+    console.log(data[123]);
 
-    });
+    for (var i = 0; i < data.length; i++) {
+      $select.append('<option id="' + data[i]['Id'] + '">' + data[i]['School'] + '</option>');
+    }
+
   });
+});
 
+
+-function post() {
+  var http = new XMLHttpRequest();
+  var url = "";
+  var params = "data=hello";
+  http.open("POST", url, true);
+  http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  http.onreadystatechange = function () {//Call a function when the state changes.
+    if (http.readyState == 4 && http.status == 200) {
+      alert(http.responseText);
+    }
+  }
+  http.send(params);
+  console.info("post send");
+}
